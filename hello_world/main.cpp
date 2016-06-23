77126d122ee7ab1e6c88906fc77edd2ab5638d7a
nagihwan <ngh0818@nexon.co.kr>
2016-06-23 17:19:41 +0900

    add 'add' function

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+int add(int a, int b)
+{
+	return a + b;
+}
+
 int main(void)
 {
 	printf("Hello World\n");
 
-	printf("This is NxK Repository\n");
+	printf("This is NxA Repository\n");
+
+	printf("3 + 5 = %d\n", add(3, 5));
 
 	return 0;
 }
